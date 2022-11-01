5dab5adddfc1d035989ab4dc43ef02355918a8ad
Guilllermo Oscar <85650228+GuillermoOsc@users.noreply.github.com>
2022-10-31 23:36:47 -0300

    commit 002

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 #### Capacitación con Jest: Framework de Javascript para pruebas unitarias.
+
+- Ejemplos de matchers personalizados en https://github.com/jest-community/jest-extended
+
+- Ejemplos de matchers personalizados para DOM (frontend) https://github.com/testing-library/jest-dom
+
+- CLI Jest https://jestjs.io/es-ES/docs/cli#--coverageboolean
--- src/jest-extend.test.js
+++ src/jest-extend.test.js
@@ -1,2 +1,24 @@
-// Se crean matchers personalizados ej. toBeprueba, toBeprueba2, etc.
+// Se crean matchers personalizados ej. toBePruebaPersonalizada, toBePrueba2, etc.
 
+// https://github.com/jest-community/jest-extended
+
+// Ejemplo de un matchers personalizado
+
+expect.extend ({
+    tobeIgualDos(recibido) {
+        if (recibido !== 2) {
+            return {
+                pass: false,
+                message: () => `Espera ${received} ser el número 2`,
+            };
+        }
+
+        return {
+            pass: true,
+        };
+    },
+});
+
+test (" número 2", ()=> {
+    expect(2).tobeIgualDos();
+});
